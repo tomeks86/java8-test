3d1d7714047a1d9fad166748ff71f963c6a755f1
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-06-02 18:04:43 +0200

    words in a book sorting in Java 8

---
+++ src/main/java/edu/tseidler/Main.java
@@ -0,0 +1,46 @@
+package edu.tseidler;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		Path path = Paths.get((Main.class.getResource("book.txt").toURI()));
+		System.out.println(path.toString());
+		// Files.lines(path, Charset.forName("Cp1252")).forEach(System.out::println);
+		// System.exit(0);
+		Stream<String> lines = Files.lines(path, Charset.forName("Cp1252"));
+		Long t1 = Instant.now().toEpochMilli();
+		Map<String, Long> words = lines //
+//				.parallel() //
+				.map(l -> l.split("\\s+")) //
+				.flatMap(Arrays::stream) //
+				.filter(w -> !w.isEmpty()) //
+				.map(w -> w.replaceAll("\\W", ""))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		Long t2 = Instant.now().toEpochMilli();
+		System.out.println(words.size());
+		words.entrySet().stream() //
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) //
+				.limit(50) //
+				.forEach(e -> System.out.printf("%s: %s\n", e.getKey(), e.getValue()));
+		
+		Long t3 = Instant.now().toEpochMilli();
+		
+		System.out.printf("total time: %d\n", t3 - t1);
+		System.out.printf("reading time: %d\n", t2 - t1);
+		System.out.printf("sorting time: %d\n", t3 - t2);
+	}
+}
